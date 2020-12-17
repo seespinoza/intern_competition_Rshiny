5168af218940304f1e5a4d5ef350eddd7d44c9e4
Sebastian Espinoza <srespinoza@catchintelligence.com>
2020-12-17 12:04:27 -0600

    interactive script

--- plotly_quarters.R
+++ plotly_quarters.R
@@ -1,43 +1,82 @@
-library(TTR)
+library(shiny)
 library(plotly)
+library(TTR)
 res = read.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
 
-# Get data for US only
-usRSI <- res[res[,'location'] == 'United States',]
+# User Interface
+ui <- fluidPage(
+  
+  # Retrieve input
+  selectizeInput(
+    
+    # Country input variable
+    inputId = "country", 
+    label = "Select a Country", 
+    choices = unique(res$location), 
+    selected = "Algeria",
+    multiple = FALSE
+  ),
+  plotlyOutput(outputId = "p")
+)
 
-# Calculate RSI using 14 day periods
-usRSI['RSI'] <- RSI(usRSI$new_cases, n = 14)
+# Server variable
+server <- function(input, output, ...) {
+  
+  # Calculate RSI
+  locationRSI <- reactive({
+    # Filter data by country
+    df <- res[res[,'location'] == input$country,]
+    
+    # Calculate RSI
+    df['RSI'] <- RSI(df$new_cases, n = 14)
+    return(df)
+  })
+  
+  # Create background shapes
+  createShapes <- reactive({
+    
+    # Shape color
+    shape = list(type = 'rect', line = list(color = 'rgba(0,0,0,0)'), 
+                 fillcolor = 'rgba(147,112,219,0.1)', xref='x', yref='y')
+    
+    shapes <- list()
+    
+    # Q1
+    shape[['x0']] <- 0
+    shape[['x1']] <- 69
+    shape[['y0']] <- 0
+    shape[['y1']] <- 95
+    shapes <- c(shapes, list(shape))
+    
+    # Q3
+    shape[['x0']] <- 69 + 91
+    shape[['x1']] <- 69 + 91 + 92
+    shape[['y0']] <- 0
+    shape[['y1']] <- 95
+    shapes <- c(shapes, list(shape))
+    
+    return(shapes)
+  })
+  
+  
+  output$p <- renderPlotly({
+    
+    # Create plot
+    fig <- plot_ly(locationRSI(), x = ~date, y= ~RSI, type='scatter', 
+                   mode = 'lines', name = 'Relative Strenth Index (RSI)') %>%
+      layout(title = 'Total Covid Cases 2020', 
+             xaxis = list(title = 'Date'),
+             yaxis = list(title = 'RSI'),
+             shapes=createShapes()) %>% 
+      add_trace(x = ~date, y = ~total_cases, mode = 'lines', 
+                yaxis = 'y2', name = 'Total Cases' ) %>%
+      layout(yaxis2 = list(overlaying = 'y', side = 'right',
+                           title = 'Total Cases'), hovermode = 'compare')
+    
+    return(fig)
+    
+  })
+}
 
-
-# Plot RSI 
-fig <- plot_ly(usRSI, x= ~date, y= ~RSI, type='scatter', mode = 'lines',
-               name = 'RSI') %>%
-  # Include total cases with separate y axis on right
-  add_trace(x = ~date, y = ~total_cases, mode = 'lines', 
-            yaxis = 'y2', name = 'Total_Cases' ) %>%
-  layout(yaxis2 = list(overlaying = 'y', side = 'right', yaxis = list('Total Cases')), hovermode = 'compare')
-
-
-# set up shapes
-
-shape = list(type = 'rect', line = list(color = 'rgba(0,0,0,0)'), 
-             fillcolor = 'rgba(147,112,219,0.1)', xref='x', yref='y')
-shape_offset = 0.5
-shapes <- list()
-
-# Q1
-shape[['x0']] <- 0
-shape[['x1']] <- 69
-shape[['y0']] <- 0
-shape[['y1']] <- 95
-shapes <- c(shapes, list(shape))
-
-# Q3
-shape[['x0']] <- 69 + 91
-shape[['x1']] <- 69 + 91 + 92
-shape[['y0']] <- 0
-shape[['y1']] <- 95
-shapes <- c(shapes, list(shape))
-
-fig <- layout(fig, shapes=shapes)
-fig
+# Run shiny app
+shinyApp(ui, server)
